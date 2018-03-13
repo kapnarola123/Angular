46d09e2b17f60cdf0ad18fa2960471956261e14a
Kamlesh Pokiya <kap@narola.email>
2018-03-13 19:37:03 +0530

    Componet and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./assets/css/bootstrap.css',
+    './assets/font-awesome/css/font-awesome.css', 
+    './assets/css/zabuto_calendar.css', 
+    './assets/css/style.css', 
+    './assets/css/style-responsive.css'],
+
 })
 export class AppComponent {
   title = 'app';
